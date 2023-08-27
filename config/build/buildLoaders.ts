5de2da5c881fa2a4a847430e5fc9bf3736d63a0a
Tatiana Tikhonova <tanianova@yandex.ru>
2023-08-27 15:48:17 +0300

    initial commit

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,36 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
+
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+
+    // если не используем TS - нужен babel-loader
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader, // Creates `style` nodes from JS strings
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]'
+                    },
+                }
+            }, // Translates CSS into CommonJS
+            "sass-loader", // Compiles Sass to CSS
+        ],
+    }
+
+    // важна последовательность лоадеров
+    return [
+        typescriptLoader,
+        cssLoader
+    ]
+}
