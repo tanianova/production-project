f8bb8236cd9aa672fbfb1e254cbb6bf9f0880289
Tatiana Tikhonova <tanianova@yandex.ru>
2023-09-24 10:46:47 +0300

    added stories for input and loginForm. added pre commit (husky)

---
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import { LoginForm } from './LoginForm';
+
+export default {
+    title: 'features/LoginForm',
+    component: LoginForm,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as Meta<typeof LoginForm>;
+
+const Template: StoryFn<typeof LoginForm> = (args) => <LoginForm {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
