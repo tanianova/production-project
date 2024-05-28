a195237337302f6cd3af6714595d9dea7a0ae2f3
Tatiana Tikhonova <tanianova@yandex.ru>
2024-05-28 10:07:38 +0300

    added fetching articles, view type selector

---
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -0,0 +1,45 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import ListIcon from 'shared/assets/icons/list-24-24.svg';
+import TiledIcon from 'shared/assets/icons/tiled-24-24.svg';
+import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Icon } from 'shared/ui/Icon/Icon';
+import cls from './ArticleViewSelector.module.scss';
+import { ArticleView } from '../../model/types/article';
+
+interface ArticleViewSelectorProps {
+    className?: string;
+    view:ArticleView;
+    onViewClick?:(view:ArticleView)=>void
+}
+
+const viewTypes = [
+    {
+        view: ArticleView.SMALL,
+        icon: TiledIcon,
+    }, {
+        view: ArticleView.BIG,
+        icon: ListIcon,
+    },
+];
+
+export const ArticleViewSelector = memo((props: ArticleViewSelectorProps) => {
+    const { className, view, onViewClick } = props;
+    const { t } = useTranslation();
+
+    // используем замыкание для вызова функции, что вернет нам новую функцию
+    const onClick = (newView:ArticleView) => () => {
+        onViewClick?.(newView); // попадает в слушателя события onClick
+    };
+
+    return (
+        <div className={classNames(cls.ArticleViewSelector, {}, [className])}>
+            {viewTypes.map((viewType) => (
+                <Button theme={ButtonTheme.CLEAR} onClick={onClick(viewType.view)}>
+                    <Icon Svg={viewType.icon} className={classNames('', { [cls.notSelected]: viewType.view !== view })} />
+                </Button>
+            ))}
+        </div>
+    );
+});
