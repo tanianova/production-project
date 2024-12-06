e22559b145ef93b19234263a95e6958a957b402e
Tatiana Tikhonova <tanianova@yandex.ru>
2024-12-06 20:37:23 +0300

    added force update (костыль)

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { ErrorBoundary } from '@/app/providers/ErrorBoundary';
 import { App } from './app/App';
 import '@/app/styles/index.scss';
 import './shared/config/i18n/i18n';
+import { ForceUpdateProvider } from '@/shared/lib/render/forceUpdate';
 
 const container = document.getElementById('root');
 
@@ -22,9 +23,11 @@ root.render(
     <BrowserRouter>
         <StoreProvider>
             <ErrorBoundary>
-                <ThemeProvider>
-                    <App />
-                </ThemeProvider>
+                <ForceUpdateProvider>
+                    <ThemeProvider>
+                        <App />
+                    </ThemeProvider>
+                </ForceUpdateProvider>
             </ErrorBoundary>
         </StoreProvider>
     </BrowserRouter>,
